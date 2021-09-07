5386956fa19b4fd2926121cbf980f2a756bba88a
Dominic <farfetchedking541@gmail.com>
2021-09-06 19:13:49 -0500

    Ravager body part problem fixes

---
+++ FuckYouModeAIs/Ravager/RavagerLegRightBehaviorOverride.cs
@@ -0,0 +1,37 @@
+﻿using CalamityMod.NPCs;
+using CalamityMod.NPCs.Ravager;
+using InfernumMode.OverridingSystem;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InfernumMode.FuckYouModeAIs.Ravager
+{
+    public class RavagerLegRightBehaviorOverride : NPCBehaviorOverride
+    {
+        public override int NPCOverrideType => ModContent.NPCType<RavagerLegRight>();
+
+        public override NPCOverrideContext ContentToOverride => NPCOverrideContext.NPCAI;
+
+        public override bool PreAI(NPC npc)
+        {
+            // Fuck off if the main boss is gone.
+            if (CalamityGlobalNPC.scavenger < 0 || !Main.npc[CalamityGlobalNPC.scavenger].active)
+            {
+                npc.active = false;
+                npc.netUpdate = true;
+                return false;
+            }
+
+            NPC ravager = Main.npc[CalamityGlobalNPC.scavenger];
+
+            if (npc.timeLeft < 1800)
+                npc.timeLeft = 1800;
+
+            npc.Center = ravager.Center + new Vector2(70f, 88f);
+            npc.Opacity = ravager.Opacity;
+
+            return false;
+        }
+    }
+}

b2f2afa9af21f2486c2c657f2876662cc8af829b
NycroV <83246959+NycroV@users.noreply.github.com>
2021-09-17 23:59:13 -0400

    Change Astrum Deus namespace so GitHub will get off my dick

--- FuckYouModeAIs/AstrumDeus/DeusSpawnerBehaviorOverride.cs
+++ FuckYouModeAIs/AstrumDeus/DeusSpawnerBehaviorOverride.cs
@@ -8,7 +8,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace InfernumMode.FuckYouModeAIs.MoonLord
+namespace InfernumMode.FuckYouModeAIs.AstrumDeus
 {
     public class DeusSpawnerBehaviorOverride : ProjectileBehaviorOverride
     {

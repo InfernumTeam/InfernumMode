75558aff0f6f19711999faf791c6482e7f72bcaf
Dominic <farfetchedking541@gmail.com>
2021-09-20 01:36:43 -0500

    Very bad bug fix

--- UtilityMethods/GlobalExtensions.cs
+++ UtilityMethods/GlobalExtensions.cs
@@ -6,7 +6,7 @@ namespace InfernumMode
     public static partial class Utilities
     {
         public static PoDPlayer Infernum(this Player player) => player.GetModPlayer<PoDPlayer>();
-        public static GlobalNPCOverrides Infernum(this NPC npc) => npc.Infernum();
+        public static GlobalNPCOverrides Infernum(this NPC npc) => npc.GetGlobalNPC<GlobalNPCOverrides>();
         public static GlobalProjectileOverrides Infernum(this Projectile projectile) => projectile.GetGlobalProjectile<GlobalProjectileOverrides>();
     }
 }

8148f93f7cbe2a474453f375c7343c6d62cfef42
Dominic <farfetchedking541@gmail.com>
2021-09-24 13:53:02 -0500

    Golem AI overrides base

---
+++ BehaviorOverrides/BossAIs/Golem/GolemFistLeftBehaviorOverride.cs
@@ -0,0 +1,20 @@
+﻿using InfernumMode.OverridingSystem;
+using Terraria;
+using Terraria.ID;
+
+namespace InfernumMode.BehaviorOverrides.BossAIs.Golem
+{
+    public class GolemFistLeftBehaviorOverride : NPCBehaviorOverride
+    {
+        public override int NPCOverrideType => NPCID.GolemFistLeft;
+
+        public override NPCOverrideContext ContentToOverride => NPCOverrideContext.NPCAI;
+
+        public override bool PreAI(NPC npc) => DoClawAI(npc, true);
+
+        public static bool DoClawAI(NPC npc, bool leftClaw)
+        {
+            return false;
+        }
+    }
+}
---
+++ BehaviorOverrides/BossAIs/Golem/GolemFistRightBehaviorOverride.cs
@@ -0,0 +1,15 @@
+﻿using InfernumMode.OverridingSystem;
+using Terraria;
+using Terraria.ID;
+
+namespace InfernumMode.BehaviorOverrides.BossAIs.Golem
+{
+    public class GolemFistRightBehaviorOverride : NPCBehaviorOverride
+    {
+        public override int NPCOverrideType => NPCID.GolemFistRight;
+
+        public override NPCOverrideContext ContentToOverride => NPCOverrideContext.NPCAI;
+
+        public override bool PreAI(NPC npc) => GolemFistLeftBehaviorOverride.DoClawAI(npc, false);
+    }
+}

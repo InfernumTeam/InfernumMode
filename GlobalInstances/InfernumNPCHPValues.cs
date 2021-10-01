d51062146f03a00342480d9bc1fc38ebf557d8d3
Dominic <farfetchedking541@gmail.com>
2021-09-30 21:03:11 -0500

    Made Medium Perf Bodies have less max HP

--- GlobalInstances/InfernumNPCHPValues.cs
+++ GlobalInstances/InfernumNPCHPValues.cs
@@ -34,6 +34,7 @@ namespace InfernumMode.GlobalInstances
             [ModContent.NPCType<HiveMindP2>()] = 4695,
             [ModContent.NPCType<PerforatorHive>()] = 6132,
             [ModContent.NPCType<PerforatorHeadSmall>()] = 2280,
+            [ModContent.NPCType<PerforatorBodyMedium>()] = 110,
             [ModContent.NPCType<PerforatorHeadLarge>()] = 4055,
             [NPCID.QueenBee] = 7550,
             [NPCID.SkeletronHead] = 5517,

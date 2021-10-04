1831a7e30f8df78ba3a3c8f906fe3b0617e63966
Dominic <farfetchedking541@gmail.com>
2021-10-03 20:28:39 -0500

    Work on P2 SW

---
+++ BehaviorOverrides/BossAIs/StormWeaver/ElectricOrb.cs
@@ -0,0 +1,63 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace InfernumMode.BehaviorOverrides.BossAIs.StormWeaver
+{
+    public class ElectricOrb : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Orb");
+        }
+
+        public override void SetDefaults()
+        {
+            Main.projFrames[projectile.type] = 6;
+            projectile.width = projectile.height = 88;
+            projectile.penetrate = -1;
+            projectile.tileCollide = false;
+            projectile.timeLeft = 90;
+            projectile.Opacity = 0f;
+        }
+
+        public override void AI()
+        {
+            projectile.Opacity = Utils.InverseLerp(90f, 65f, projectile.timeLeft, true) * Utils.InverseLerp(0f, 25f, projectile.timeLeft, true);
+            projectile.frameCounter++;
+            projectile.frame = projectile.frameCounter / 5 % Main.projFrames[projectile.type];
+            projectile.velocity *= 0.98f;
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return new Color(255, 255, 255, 56) * projectile.Opacity;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Player target = Main.player[Player.FindClosest(projectile.Center, 1, 1)];
+            Main.PlaySound(SoundID.DD2_KoboldExplosion, projectile.Center);
+            if (Main.netMode == NetmodeID.MultiplayerClient)
+                return;
+
+            // Release a lightning bolt and circle of sparks.
+            Vector2 lightningVelocity = projectile.SafeDirectionTo(target.Center) * (projectile.Distance(target.Center) / 450f + 6.1f);
+            int arc = Utilities.NewProjectileBetter(projectile.Center, lightningVelocity, ProjectileID.CultistBossLightningOrbArc, 255, 0f);
+            if (Main.projectile.IndexInRange(arc))
+            {
+                Main.projectile[arc].ai[0] = lightningVelocity.ToRotation();
+                Main.projectile[arc].ai[1] = Main.rand.Next(100);
+                Main.projectile[arc].tileCollide = false;
+            }
+
+            float initialSparkRotation = Main.rand.NextFloat(MathHelper.TwoPi);
+            for (int i = 0; i < 10; i++)
+            {
+                Vector2 sparkVelocity = (MathHelper.TwoPi * i / 10f + initialSparkRotation).ToRotationVector2() * 15f;
+                Utilities.NewProjectileBetter(projectile.Center + sparkVelocity * 3f, sparkVelocity, ModContent.ProjectileType<WeaverSpark>(), 255, 0f);
+            }
+        }
+    }
+}
---
+++ BehaviorOverrides/BossAIs/StormWeaver/StormWeaverSegmentBehaviorOverride.cs
@@ -0,0 +1,63 @@
+﻿using CalamityMod;
+using CalamityMod.NPCs.StormWeaver;
+using InfernumMode.OverridingSystem;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InfernumMode.BehaviorOverrides.BossAIs.StormWeaver
+{
+    public class StormWeaverBodyBehaviorOverride : NPCBehaviorOverride
+    {
+        public override int NPCOverrideType => ModContent.NPCType<StormWeaverBodyNaked>();
+
+        public override NPCOverrideContext ContentToOverride => NPCOverrideContext.NPCAI;
+
+        public static void SegmentAI(NPC npc)
+        {
+            NPC aheadSegment = Main.npc[(int)npc.ai[1]];
+            NPC head = Main.npc[(int)npc.ai[2]];
+            if (!aheadSegment.active)
+            {
+                npc.life = 0;
+                npc.HitEffect();
+                npc.active = false;
+                npc.netUpdate = true;
+                return;
+            }
+
+            // Inherit various attributes from the head segment.
+            // This code will go upstream across every segment, until it reaches the head.
+            npc.scale = aheadSegment.scale;
+            npc.life = head.life;
+            npc.lifeMax = head.lifeMax;
+            npc.Opacity = head.Opacity;
+            npc.Calamity().DR = head.Calamity().DR;
+
+            Vector2 directionToNextSegment = aheadSegment.Center - npc.Center;
+            if (aheadSegment.rotation != npc.rotation)
+                directionToNextSegment = directionToNextSegment.RotatedBy(MathHelper.WrapAngle(aheadSegment.rotation - npc.rotation) * 0.05f);
+
+            npc.rotation = directionToNextSegment.ToRotation() + MathHelper.PiOver2;
+            npc.Center = aheadSegment.Center - directionToNextSegment.SafeNormalize(Vector2.Zero) * 44f;
+        }
+
+        public override bool PreAI(NPC npc)
+        {
+            SegmentAI(npc);
+            return false;
+        }
+    }
+    public class StormWeaverTailBehaviorOverride : NPCBehaviorOverride
+    {
+        public override int NPCOverrideType => ModContent.NPCType<StormWeaverTailNaked>();
+
+        public override NPCOverrideContext ContentToOverride => NPCOverrideContext.NPCAI;
+
+        public override bool PreAI(NPC npc)
+        {
+            StormWeaverBodyBehaviorOverride.SegmentAI(npc);
+            return false;
+        }
+    }
+}

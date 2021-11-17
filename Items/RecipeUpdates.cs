4fd0882ead9432ca13f51d2e666be6c3c8fd4be7
DominicKarma <51094361+DominicKarma@users.noreply.github.com>
2021-11-17 12:11:49 -0600

    Recipe updates

--- Items/RecipeUpdates.cs
+++ Items/RecipeUpdates.cs
@@ -36,50 +36,6 @@ namespace InfernumMode.Items
 
         internal static void IncreaseBossSummonerYields()
         {
-            // Make slime crown's recipe give 10 summon items instead of 1.
-            SetRecipeResultStack(ItemID.SlimeCrown, 10);
-
-            // Make desert medallian's recipe give 5 summon items instead of 1.
-            SetRecipeResultStack(ModContent.ItemType<DriedSeafood>(), 5);
-
-            // Make suspicious looking eye's recipe give 3 summon items instead of 1.
-            SetRecipeResultStack(ItemID.SuspiciousLookingEye, 10);
-
-            // Make decapodita sprout's recipe give 5 summon items instead of 1.
-            SetRecipeResultStack(ModContent.ItemType<DecapoditaSprout>(), 5);
-
-            // Make worm food's recipe give 6 summon items instead of 1.
-            SetRecipeResultStack(ItemID.WormFood, 6);
-
-            // Make bloody spine's recipe give 6 summon items instead of 1.
-            SetRecipeResultStack(ItemID.BloodySpine, 6);
-
-            // Make teratoma's recipe give 6 summon items instead of 1.
-            SetRecipeResultStack(ModContent.ItemType<Teratoma>(), 6);
-
-            // Make bloody worm food's recipe give 6 summon items instead of 1.
-            SetRecipeResultStack(ModContent.ItemType<BloodyWormFood>(), 6);
-
-            // Make abeemination's recipe give 8 summon items instead of 1.
-            SetRecipeResultStack(ItemID.Abeemination, 8);
-
-            // Make overloaded sludge's recipe give 4 summon items instead of 1.
-            SetRecipeResultStack(ModContent.ItemType<OverloadedSludge>(), 4);
-
-            // Make cryo key's recipe give 6 summon items instead of 1.
-            SetRecipeResultStack(ModContent.ItemType<CryoKey>(), 6);
-
-            // Make charred idol's recipe give 6 summon items instead of 1.
-            SetRecipeResultStack(ModContent.ItemType<CharredIdol>(), 6);
-
-            // Make seafood's recipe give 4 summon items instead of 1.
-            SetRecipeResultStack(ModContent.ItemType<Seafood>(), 4);
-
-            // Make mechanical summoner's recipes give 8 summon items instead of 1.
-            SetRecipeResultStack(ItemID.MechanicalEye, 8);
-            SetRecipeResultStack(ItemID.MechanicalSkull, 8);
-            SetRecipeResultStack(ItemID.MechanicalWorm, 8);
-
             // Remove ingredients from boss summoners that require the boss be defeated.
             RemoveRecipeIngredient(ModContent.ItemType<Teratoma>(), ModContent.ItemType<TrueShadowScale>());
             RemoveRecipeIngredient(ModContent.ItemType<BloodyWormFood>(), ModContent.ItemType<BloodSample>());

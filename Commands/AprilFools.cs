5d25d16f2765b8471dd0a0969a9522f82f1fc7fa
NycroV <83246959+NycroV@users.noreply.github.com>
2021-11-28 04:16:37 -0500

    April fools override utility

---
+++ Commands/AprilFools.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+
+namespace InfernumMode.Commands
+{
+    class AprilFools : ModCommand
+    {
+        public override CommandType Type => CommandType.Chat;
+        public override string Command => "aprilfools";
+        public override string Description => "Toggle an april fools override";
+        public override void Action(CommandCaller caller, string input, string[] args)
+        {
+            Utilities.OverrideAprilFirst = !Utilities.OverrideAprilFirst;
+            Main.NewText($"April fools override set to: {Utilities.OverrideAprilFirst}", Color.DarkTurquoise);
+        }
+    }
+}
--- UtilityMethods/TemporalUtilities.cs
+++ UtilityMethods/TemporalUtilities.cs
@@ -4,6 +4,8 @@ namespace InfernumMode
 {
     public static partial class Utilities
     {
-        public static bool IsAprilFirst() => DateTime.Now.Month == 4 && DateTime.Now.Day == 1;
+        public static bool OverrideAprilFirst = false;
+
+        public static bool IsAprilFirst() => (DateTime.Now.Month == 4 && DateTime.Now.Day == 1) || OverrideAprilFirst;
     }
 }

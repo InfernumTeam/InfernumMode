2a770cd6b6c0a968ae270ac4fa73b769e288d264
NycroV <83246959+NycroV@users.noreply.github.com>
2022-03-23 00:21:05 -0400

    Golem Framework

--- BehaviorOverrides/BossAIs/Golem/GolemFreeHeadBehaviorOverride.cs
+++ BehaviorOverrides/BossAIs/Golem/GolemFreeHeadBehaviorOverride.cs
@@ -15,6 +15,11 @@ namespace InfernumMode.BehaviorOverrides.BossAIs.Golem
 
         public override bool PreAI(NPC npc)
         {
+            if (!Main.npc[(int)npc.ai[0]].active || Main.npc[(int)npc.ai[0]].type != NPCID.Golem)
+            {
+                GolemBodyBehaviorOverride.DespawnNPC(npc.whoAmI);
+                return false;
+            }
             npc.chaseable = !npc.dontTakeDamage;
             npc.Opacity = npc.dontTakeDamage ? 0f : 1f;
             return false;
@@ -26,8 +31,10 @@ namespace InfernumMode.BehaviorOverrides.BossAIs.Golem
                 return false;
 
             Texture2D texture = ModContent.GetTexture("InfernumMode/BehaviorOverrides/BossAIs/Golem/FreeHead");
+            Texture2D glowMask = ModContent.GetTexture("InfernumMode/BehaviorOverrides/BossAIs/Golem/FreeHeadGlow");
             Rectangle rect = new Rectangle(0, 0, texture.Width, texture.Height);
-            Main.spriteBatch.Draw(texture, npc.Center - Main.screenPosition, rect, lightColor, npc.rotation, npc.Center, 1f, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(texture, npc.Center - Main.screenPosition, rect, lightColor * npc.Opacity, npc.rotation, rect.Size() * 0.5f, 1f, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(glowMask, npc.Center - Main.screenPosition, rect, Color.White * npc.Opacity, npc.rotation, rect.Size() * 0.5f, 1f, SpriteEffects.None, 0f);
             GolemHeadBehaviorOverride.DoEyeDrawing(npc);
             return false;
         }
--- BehaviorOverrides/BossAIs/Golem/GolemHeadBehaviorOverride.cs
+++ BehaviorOverrides/BossAIs/Golem/GolemHeadBehaviorOverride.cs
@@ -1,6 +1,7 @@
 ﻿using InfernumMode.OverridingSystem;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
@@ -17,9 +18,13 @@ namespace InfernumMode.BehaviorOverrides.BossAIs.Golem
 
         private static Dictionary<GolemAttackState, Color> AttackEyeColorPairs = new Dictionary<GolemAttackState, Color>
         {
+            [GolemAttackState.SummonDelay] = Color.Transparent,
+            [GolemAttackState.BIGSHOT] = Color.Transparent,
+            [GolemAttackState.BadTime] = Color.Transparent,
+
             [GolemAttackState.ArmBullets] = Color.AntiqueWhite,
-            [GolemAttackState.FistSpin] = Color.Red,
-            [GolemAttackState.HeatRay] = Color.Orange,
+            [GolemAttackState.FistSpin] = Color.Orange,
+            [GolemAttackState.HeatRay] = Color.Aquamarine,
             [GolemAttackState.SpikeTrapWaves] = Color.LightBlue,
             [GolemAttackState.SpinLaser] = Color.Firebrick,
             [GolemAttackState.Slingshot] = Color.MediumPurple,
@@ -27,19 +32,35 @@ namespace InfernumMode.BehaviorOverrides.BossAIs.Golem
 
         public override bool PreAI(NPC npc)
         {
+            if (!Main.npc[(int)npc.ai[0]].active || Main.npc[(int)npc.ai[0]].type != NPCID.Golem)
+            {
+                GolemBodyBehaviorOverride.DespawnNPC(npc.whoAmI);
+                return false;
+            }
+            NPCID.Sets.MustAlwaysDraw[NPCID.GolemHead] = true;
             npc.chaseable = !npc.dontTakeDamage;
-            npc.Opacity = npc.dontTakeDamage ? 0f : 1f;
+            if (Main.npc[(int)npc.ai[0]].ai[0] > 242f)
+                npc.Opacity = npc.dontTakeDamage ? 0f : 1f;
             return false;
         }
 
         public override bool PreDraw(NPC npc, SpriteBatch spriteBatch, Color lightColor)
         {
-            if (npc.dontTakeDamage)
+            // Only draw eyes on summon animation
+            if (Main.npc[(int)npc.ai[0]].ai[0] < 182f)
+            {
+                DoEyeDrawing(npc);
+                return false;
+            }
+
+            if (npc.Opacity == 0f && Main.npc[(int)npc.ai[0]].ai[0] > 242)
                 return false;
 
             Texture2D texture = ModContent.GetTexture("InfernumMode/BehaviorOverrides/BossAIs/Golem/AttachedHead");
+            Texture2D glowMask = ModContent.GetTexture("InfernumMode/BehaviorOverrides/BossAIs/Golem/AttachedHeadGlow");
             Rectangle rect = new Rectangle(0, 0, texture.Width, texture.Height);
-            Main.spriteBatch.Draw(texture, npc.Center - Main.screenPosition, rect, lightColor, npc.rotation, npc.Center, 1f, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(texture, npc.Center - Main.screenPosition, rect, lightColor * npc.Opacity, npc.rotation, rect.Size() * 0.5f, 1f, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(glowMask, npc.Center - Main.screenPosition, rect, Color.White * npc.Opacity, npc.rotation, rect.Size() * 0.5f, 1f, SpriteEffects.None, 0f);
             DoEyeDrawing(npc);
             return false;
         }
@@ -49,17 +70,70 @@ namespace InfernumMode.BehaviorOverrides.BossAIs.Golem
             Texture2D texture = ModContent.GetTexture("InfernumMode/BehaviorOverrides/BossAIs/Golem/GolemEyeGlow");
             Rectangle rect = new Rectangle(0, 0, texture.Width, texture.Height);
             float rotation = MathHelper.Lerp(0f, MathHelper.TwoPi, npc.ai[1] / 240f);
-            float rotation2 = Utils.InverseLerp(0f, MathHelper.TwoPi, npc.ai[1] / 240f);
-            Color drawColor = AttackEyeColorPairs[(GolemAttackState)Main.npc[(int)npc.ai[0]].ai[1]] * 0.75f;
-            drawColor.A = 0;
-            Vector2 drawPos = npc.type == NPCID.GolemHead ? new Vector2(npc.Center.X - 15f, npc.Bottom.Y + 50f) : new Vector2(npc.Center.X - 15f, npc.Bottom.Y + 78f);
+            float rotation2 = MathHelper.Lerp(MathHelper.TwoPi, 0f, npc.ai[1] / 240f);
+            Color nextColor = AttackEyeColorPairs[(GolemAttackState)Main.npc[(int)npc.ai[0]].ai[1]];
+            nextColor.A = 0;
+            Vector2 drawPos = npc.type == NPCID.GolemHead ? new Vector2(npc.Center.X - 15f, npc.Bottom.Y - 33f) : new Vector2(npc.Center.X - 15f, npc.Bottom.Y - 57f);
             Vector2 drawPos2 = new Vector2(drawPos.X + 30f, drawPos.Y);
+
+            NPC body = Main.npc[(int)npc.ai[0]];
+
+            if ((GolemAttackState)body.ai[1] == GolemAttackState.BIGSHOT || (GolemAttackState)body.ai[1] == GolemAttackState.BadTime || (GolemAttackState)body.ai[1] == GolemAttackState.SummonDelay)
+            {
+                float DarknessRatio = body.Infernum().ExtraAI[9];
+                Color leftColor;
+                Color rightColor;
+
+                if ((GolemAttackState)body.ai[1] == GolemAttackState.BIGSHOT)
+                {
+                    leftColor = Color.LightPink;
+                    rightColor = Color.Yellow;
+                }
+                else if ((GolemAttackState)body.ai[1] == GolemAttackState.BadTime)
+                {
+                    leftColor = Color.Transparent;
+                    rightColor = Color.Cyan;
+                }
+                else
+                {
+                    leftColor = Color.Red;
+                    rightColor = Color.Red;
+                }
+
+                float colorRatio;
+                if (body.ai[0] > 120f)
+                    colorRatio = 1 - MathHelper.Clamp((body.ai[0] - 121f) / 30f, 0f, 1f);
+                else
+                    colorRatio = 0f;
+
+                Texture2D black = ModContent.GetTexture("InfernumMode/BehaviorOverrides/BossAIs/Golem/BlackPixel");
+                float blackScale = Math.Max(Main.screenWidth, Main.screenHeight);
+                Vector2 blackPos = new Vector2(Main.screenWidth / 2f, Main.screenHeight / 2f);
+                Main.spriteBatch.Draw(black, blackPos, new Rectangle(0, 0, 1, 1), Color.White * DarknessRatio, 0f, new Vector2(.5f, .5f), blackScale, SpriteEffects.None, 0f);
+
+                for (float i = 4; i > 0; i--)
+                {
+                    float scale = i / 4f;
+                    Main.spriteBatch.Draw(texture, drawPos - Main.screenPosition, rect, leftColor * colorRatio, rotation, rect.Size() * 0.5f, scale, SpriteEffects.None, 0f);
+                    Main.spriteBatch.Draw(texture, drawPos2 - Main.screenPosition, rect, rightColor * colorRatio, rotation2, rect.Size() * 0.5f, scale, SpriteEffects.None, 0f);
+                    Main.spriteBatch.Draw(texture, drawPos - Main.screenPosition, rect, leftColor * colorRatio, rotation + MathHelper.PiOver2, rect.Size() * 0.5f, scale, SpriteEffects.None, 0f);
+                    Main.spriteBatch.Draw(texture, drawPos2 - Main.screenPosition, rect, rightColor * colorRatio, rotation2 + MathHelper.PiOver2, rect.Size() * 0.5f, scale, SpriteEffects.None, 0f);
+                }
+                return;
+            }
+
+            float AttackCooldownRatio = GolemBodyBehaviorOverride.ConstAttackCooldown - body.Infernum().ExtraAI[7];
+            Color prevColor = AttackEyeColorPairs[(GolemAttackState)body.Infernum().ExtraAI[8]];
+            float ratio = MathHelper.Clamp(AttackCooldownRatio / (GolemBodyBehaviorOverride.ConstAttackCooldown * 0.67f), 0f, 1f);
+            Color drawColor = body.Infernum().ExtraAI[5] == 1f || body.Infernum().ExtraAI[6] == 1f ? Color.Red * 0.25f : Color.Lerp(prevColor, nextColor, ratio) * 0.25f;
 
             for (float i = 4; i > 0; i--)
             {
                 float scale = i / 4f;
-                Main.spriteBatch.Draw(texture, drawPos, rect, drawColor, rotation, drawPos, scale, SpriteEffects.None, 0f);
-                Main.spriteBatch.Draw(texture, drawPos2, rect, drawColor, rotation2, drawPos2, scale, SpriteEffects.None, 0f);
+                Main.spriteBatch.Draw(texture, drawPos - Main.screenPosition, rect, drawColor, rotation, rect.Size() * 0.5f, scale, SpriteEffects.None, 0f);
+                Main.spriteBatch.Draw(texture, drawPos2 - Main.screenPosition, rect, drawColor, rotation2, rect.Size() * 0.5f, scale, SpriteEffects.None, 0f);
+                Main.spriteBatch.Draw(texture, drawPos - Main.screenPosition, rect, drawColor, rotation + MathHelper.PiOver2, rect.Size() * 0.5f, scale, SpriteEffects.None, 0f);
+                Main.spriteBatch.Draw(texture, drawPos2 - Main.screenPosition, rect, drawColor, rotation2 + MathHelper.PiOver2, rect.Size() * 0.5f, scale, SpriteEffects.None, 0f);
             }
         }
     }

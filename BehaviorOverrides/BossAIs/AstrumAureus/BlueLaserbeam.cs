d9d88bbeddce8e931d1922e4527736a6e9070e2e
Toasty <louscliff2@gmail.com>
2022-11-06 23:06:15 +0000

    Improved aureus' laser beams

--- BehaviorOverrides/BossAIs/AstrumAureus/BlueLaserbeam.cs
+++ BehaviorOverrides/BossAIs/AstrumAureus/BlueLaserbeam.cs
@@ -4,15 +4,22 @@ using InfernumMode.GlobalInstances;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
+using System;
 using System.IO;
 using Terraria;
 using Terraria.GameContent;
+using Terraria.Graphics.Shaders;
 using Terraria.ModLoader;
 
 namespace InfernumMode.BehaviorOverrides.BossAIs.AstrumAureus
 {
     public class BlueLaserbeam : BaseLaserbeamProjectile
     {
+        public PrimitiveTrail LaserDrawer
+        {
+            get;
+            set;
+        } = null;
         public int OwnerIndex => (int)Projectile.ai[1];
         public override float Lifetime => OrangeLaserbeam.LaserLifetime;
         public override Color LaserOverlayColor => Color.White;
@@ -26,7 +33,7 @@ namespace InfernumMode.BehaviorOverrides.BossAIs.AstrumAureus
 
         public override void SetDefaults()
         {
-            Projectile.width = Projectile.height = 12;
+            Projectile.width = Projectile.height = 17;
             Projectile.hostile = true;
             Projectile.penetrate = -1;
             Projectile.tileCollide = false;
@@ -60,5 +67,34 @@ namespace InfernumMode.BehaviorOverrides.BossAIs.AstrumAureus
         }
 
         public override bool? CanDamage() => Time > 35f ? null : false;
+
+        public float LaserWidthFunction(float _) => Projectile.scale * Projectile.width;
+
+        public static Color LaserColorFunction(float completionRatio)
+        {
+            float colorInterpolant = (float)Math.Sin(Main.GlobalTimeWrappedHourly * -3.2f + completionRatio * 23f) * 0.5f + 0.5f;
+            return Color.Lerp(new(109, 242, 196), new(46, 146, 143), colorInterpolant * 0.67f);
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            // This should never happen, but just in case.
+            if (Projectile.velocity == Vector2.Zero)
+                return false;
+            LaserDrawer ??= new(LaserWidthFunction, LaserColorFunction, null, GameShaders.Misc["CalamityMod:ArtemisLaser"]);
+            Vector2 laserEnd = Projectile.Center + Projectile.velocity.SafeNormalize(Vector2.UnitY) * LaserLength;
+            Vector2[] baseDrawPoints = new Vector2[8];
+            for (int i = 0; i < baseDrawPoints.Length; i++)
+                baseDrawPoints[i] = Vector2.Lerp(Projectile.Center, laserEnd, i / (float)(baseDrawPoints.Length - 1f));
+
+            // Select textures to pass to the shader, along with the electricity color.
+            GameShaders.Misc["CalamityMod:ArtemisLaser"].UseColor(187, 220, 237);
+            GameShaders.Misc["CalamityMod:ArtemisLaser"].UseImage1("Images/Extra_189");
+            GameShaders.Misc["CalamityMod:ArtemisLaser"].UseImage2("Images/Misc/Perlin");
+
+            LaserDrawer.Draw(baseDrawPoints, -Main.screenPosition, 54);
+
+            return false;
+        }
     }
 }
--- BehaviorOverrides/BossAIs/AstrumAureus/OrangeLaserbeam.cs
+++ BehaviorOverrides/BossAIs/AstrumAureus/OrangeLaserbeam.cs
@@ -4,15 +4,23 @@ using InfernumMode.GlobalInstances;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
+using System;
 using System.IO;
 using Terraria;
 using Terraria.GameContent;
+using Terraria.Graphics.Shaders;
 using Terraria.ModLoader;
 
 namespace InfernumMode.BehaviorOverrides.BossAIs.AstrumAureus
 {
     public class OrangeLaserbeam : BaseLaserbeamProjectile
     {
+        public PrimitiveTrail LaserDrawer
+        {
+            get;
+            set;
+        } = null;
+
         public int OwnerIndex => (int)Projectile.ai[1];
         public const int LaserLifetime = 132;
         public const float FullCircleRotationFactor = 0.84f;
@@ -28,7 +36,7 @@ namespace InfernumMode.BehaviorOverrides.BossAIs.AstrumAureus
 
         public override void SetDefaults()
         {
-            Projectile.width = Projectile.height = 12;
+            Projectile.width = Projectile.height = 17;
             Projectile.hostile = true;
             Projectile.alpha = 255;
             Projectile.penetrate = -1;
@@ -63,5 +71,33 @@ namespace InfernumMode.BehaviorOverrides.BossAIs.AstrumAureus
         }
 
         public override bool? CanDamage() => Time > 35f ? null : false;
+        public float LaserWidthFunction(float _) => Projectile.scale * Projectile.width;
+
+        public Color LaserColorFunction(float completionRatio)
+        {
+            float colorInterpolant = (float)Math.Sin(Main.GlobalTimeWrappedHourly * -3.2f + completionRatio * 23f) * 0.5f + 0.5f;
+            return Color.Lerp(new(255, 164, 94), new(237, 93, 83), colorInterpolant * 0.67f);
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            // This should never happen, but just in case.
+            if (Projectile.velocity == Vector2.Zero)
+                return false;
+            LaserDrawer ??= new(LaserWidthFunction, LaserColorFunction, null, GameShaders.Misc["CalamityMod:ArtemisLaser"]);
+            Vector2 laserEnd = Projectile.Center + Projectile.velocity.SafeNormalize(Vector2.UnitY) * LaserLength;
+            Vector2[] baseDrawPoints = new Vector2[8];
+            for (int i = 0; i < baseDrawPoints.Length; i++)
+                baseDrawPoints[i] = Vector2.Lerp(Projectile.Center, laserEnd, i / (float)(baseDrawPoints.Length - 1f));
+
+            // Select textures to pass to the shader, along with the electricity color.
+            GameShaders.Misc["CalamityMod:ArtemisLaser"].UseColor(187, 220, 237);
+            GameShaders.Misc["CalamityMod:ArtemisLaser"].UseImage1("Images/Extra_189");
+            GameShaders.Misc["CalamityMod:ArtemisLaser"].UseImage2("Images/Misc/Perlin");
+
+            LaserDrawer.Draw(baseDrawPoints, -Main.screenPosition, 54);
+
+            return false;
+        }
     }
 }

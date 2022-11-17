322fba16cfa6302a4adec88126e9b24ab0f93c36
Dominic <farfetchedking541@gmail.com>
2022-11-17 13:09:15 -0600

    Crabulon attack states + colosseum column

---
+++ Tiles/PalladiumColumn.cs
@@ -0,0 +1,17 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InfernumMode.Tiles
+{
+    public class PalladiumColumn : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            AddMapEntry(new Color(100, 61, 4));
+        }
+    }
+}

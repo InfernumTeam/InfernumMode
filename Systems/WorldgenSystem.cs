d707af81f3e7857a137ff846fe8b3b423def68fa
Dominic <farfetchedking541@gmail.com>
2022-12-29 15:53:02 -0600

    Lost Colosseum Entrance requests

--- Systems/WorldgenSystem.cs
+++ Systems/WorldgenSystem.cs
@@ -1,6 +1,7 @@
 using CalamityMod;
 using CalamityMod.Schematics;
 using CalamityMod.Walls;
+using CalamityMod.World;
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
@@ -19,9 +20,6 @@ namespace InfernumMode.Systems
     {
         public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
         {
-            int floatingIslandIndex = tasks.FindIndex(g => g.Name == "Floating Islands");
-            if (floatingIslandIndex != -1)
-                tasks.Insert(floatingIslandIndex, new PassLegacy("Desert Digout Area", GenerateLostColosseumEntrance));
             int finalCleanupIndex = tasks.FindIndex(g => g.Name == "Final Cleanup");
             if (finalCleanupIndex != -1)
             {
@@ -32,19 +30,50 @@ namespace InfernumMode.Systems
                     progress.Message = "Constructing a temple for an ancient goddess";
                     GenerateProfanedArena(progress, config);
                 }));
+                tasks.Insert(++finalCleanupIndex, new PassLegacy("Desert Digout Area", GenerateLostColosseumEntrance));
             }
         }
 
         public static void GenerateLostColosseumEntrance(GenerationProgress progress, GameConfiguration config)
         {
-            Point centerLeft = WorldGen.UndergroundDesertLocation.Center;
-            centerLeft.Y -= 120;
+            Point fuck = WorldGen.UndergroundDesertLocation.Center;
+            fuck.Y -= 75;
 
-            while (CalamityUtils.ParanoidTileRetrieval(centerLeft.X, centerLeft.Y).HasTile)
-                centerLeft.X++;
+            // As much as I would like to do so, I will resist the urge to write juvenile venting comments about my current frustrations with this
+            // requested feature inside of a code comment.
+            // This part creates a lumpy, circular layer of sandstone around the entrance.
+            for (int i = 0; i < 5; i++)
+            {
+                Point lumpCenter = (fuck.ToVector2() + Main.rand.NextVector2Circular(15f, 15f)).ToPoint();
+                WorldUtils.Gen(lumpCenter, new Shapes.Circle(88, 65), Actions.Chain(new GenAction[]
+                {
+                    new Modifiers.RadialDither(82f, 88f),
+                    new Actions.SetTile(TileID.Sandstone, true)
+                }));
+            }
+
+            // Carve a cave through the sandstone.
+            int caveSeed = WorldGen.genRand.Next();
+            Point cavePosition = fuck;
+            cavePosition.X -= 12;
+            cavePosition.Y += 10;
+
+            for (int dx = 0; dx < 24; dx++)
+            {
+                int perlinOffset = (int)(SulphurousSea.FractalBrownianMotion(cavePosition.X * 0.16f, cavePosition.Y * 0.16f, caveSeed, 4) * 6f);
+                cavePosition.Y += perlinOffset;
+
+                WorldUtils.Gen(cavePosition, new Shapes.Rectangle(9, 9), Actions.Chain(new GenAction[]
+                {
+                    new Actions.ClearTile(),
+                    new Actions.PlaceWall(WallID.Sandstone)
+                }));
+                cavePosition.X -= 4;
+            }
 
             bool _ = false;
-            PlaceSchematic<Action<Chest>>("LostColosseumEntrance", centerLeft, SchematicAnchor.CenterLeft, ref _);
+            fuck.X += 32;
+            PlaceSchematic<Action<Chest>>("LostColosseumEntrance", fuck, SchematicAnchor.Center, ref _);
         }
 
         public static void GenerateUndergroundJungleArea(GenerationProgress progress, GameConfiguration configuration)

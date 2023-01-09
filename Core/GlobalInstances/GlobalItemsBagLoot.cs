23ed1aeb5614cfb9657084eec52f2611025b6a49
Dominic <farfetchedking541@gmail.com>
2023-01-08 18:40:48 -0600

    Minor cleanup of extra loot code

--- Core/GlobalInstances/GlobalItemsBagLoot.cs
+++ Core/GlobalInstances/GlobalItemsBagLoot.cs
@@ -13,6 +13,14 @@ namespace InfernumMode.GlobalInstances
     {
         public override void ModifyItemLoot(Item item, ItemLoot loot)
         {
+            // Yes, this can be abused by beating a boss, grabbing the bags, and temporarily turning on Infernum for more loot.
+            // I do not care.
+            void addInfernumExclusiveItem(int itemID, int quantity)
+            {
+                var infLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
+                infLCR.Add(itemID, 1, quantity, quantity + 1);
+            }
+
             // Starter bags provide the Blasted Tophat.
             if (item.type == ModContent.ItemType<StarterBag>())
                 loot.Add(ModContent.ItemType<BlastedTophat>());
@@ -20,144 +28,92 @@ namespace InfernumMode.GlobalInstances
             // The Eater of Worlds and Brain of Cthulhu both drop 125 extra ore and 50 extra scales/tissue samples.
             if (item.type == ItemID.EaterOfWorldsBossBag)
             {
-                var eowInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                eowInfLCR.Add(ItemID.DemoniteOre, 1, 125, 126);
-                eowInfLCR.Add(ItemID.ShadowScale, 1, 50, 51);
+                addInfernumExclusiveItem(ItemID.DemoniteOre, 125);
+                addInfernumExclusiveItem(ItemID.ShadowScale, 50);
             }
             if (item.type == ItemID.BrainOfCthulhuBossBag)
             {
-                var bocInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                bocInfLCR.Add(ItemID.CrimtaneOre, 1, 125, 126);
-                bocInfLCR.Add(ItemID.TissueSample, 1, 50, 51);
+                addInfernumExclusiveItem(ItemID.CrimtaneOre, 125);
+                addInfernumExclusiveItem(ItemID.TissueSample, 50);
             }
 
             // Queen slime drops 30 extra souls of light.
             if (item.type == ItemID.QueenSlimeBossBag)
-            {
-                var qsInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                qsInfLCR.Add(ItemID.SoulofLight, 1, 30, 31);
-            }
+                addInfernumExclusiveItem(ItemID.SoulofLight, 30);
 
             // Mechs bosses drops 30 extra souls.
             if (item.type == ItemID.DestroyerBossBag)
-            {
-                var desInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                desInfLCR.Add(ItemID.SoulofMight, 1, 30, 31);
-            }
+                addInfernumExclusiveItem(ItemID.SoulofMight, 30);
+
             if (item.type == ItemID.TwinsBossBag)
-            {
-                var twinsInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                twinsInfLCR.Add(ItemID.SoulofSight, 1, 30, 31);
-            }
+                addInfernumExclusiveItem(ItemID.SoulofSight, 30);
+
             if (item.type == ItemID.SkeletronPrimeBossBag)
-            {
-                var primeInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                primeInfLCR.Add(ItemID.SoulofFright, 1, 30, 31);
-            }
+                addInfernumExclusiveItem(ItemID.SoulofFright, 30);
 
             // Calamitas Clone drops 25 extra ashes of calamity.
             if (item.type == ModContent.ItemType<CalamitasBag>())
-            {
-                var cloneInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                cloneInfLCR.Add(ModContent.ItemType<AshesofCalamity>(), 1, 25, 26);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<AshesofCalamity>(), 25);
 
             // Plantera drops 25 extra living shards.
             if (item.type == ItemID.PlanteraBossBag)
-            {
-                var plantInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                plantInfLCR.Add(ModContent.ItemType<LivingShard>(), 1, 25, 26);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<LivingShard>(), 25);
 
             // The Plaguebringer Goliath drops 25 extra infected armor plating.
             if (item.type == ModContent.ItemType<PlaguebringerGoliathBag>())
-            {
-                var pbgInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                pbgInfLCR.Add(ModContent.ItemType<InfectedArmorPlating>(), 1, 25, 26);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<InfectedArmorPlating>(), 25);
 
             // Astrum Deus drops 60 extra fragments.
             if (item.type == ModContent.ItemType<AstrumDeusBag>())
             {
-                var deusInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                deusInfLCR.Add(ItemID.FragmentSolar, 1, 60, 61);
-                deusInfLCR.Add(ItemID.FragmentStardust, 1, 60, 61);
-                deusInfLCR.Add(ItemID.FragmentNebula, 1, 60, 61);
-                deusInfLCR.Add(ItemID.FragmentVortex, 1, 60, 61);
+                addInfernumExclusiveItem(ItemID.FragmentNebula, 60);
+                addInfernumExclusiveItem(ItemID.FragmentSolar, 60);
+                addInfernumExclusiveItem(ItemID.FragmentStardust, 60);
+                addInfernumExclusiveItem(ItemID.FragmentVortex, 60);
             }
 
-            // The Moon Lord drops 600 extra luminite.
+            // The Moon Lord drops 750 extra luminite.
             if (item.type == ItemID.MoonLordBossBag)
-            {
-                var mlInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                mlInfLCR.Add(ItemID.LunarOre, 1, 600, 601);
-            }
+                addInfernumExclusiveItem(ItemID.LunarOre, 60);
 
             // The Dragonfolly drops 25 extra effulgent feathers.
             if (item.type == ModContent.ItemType<DragonfollyBag>())
-            {
-                var follyInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                follyInfLCR.Add(ModContent.ItemType<EffulgentFeather>(), 1, 25, 26);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<EffulgentFeather>(), 25);
 
             // Providence drops 30 extra divine geodes.
             if (item.type == ModContent.ItemType<ProvidenceBag>())
-            {
-                var provInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                provInfLCR.Add(ModContent.ItemType<DivineGeode>(), 1, 30, 31);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<DivineGeode>(), 30);
 
-            // Sentinels drops 8 extra of thier respective drop material.
+            // Sentinels drops 8 extra of their respective drop material.
             if (item.type == ModContent.ItemType<StormWeaverBag>())
-            {
-                var swInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                swInfLCR.Add(ModContent.ItemType<ArmoredShell>(), 1, 8, 9);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<ArmoredShell>(), 8);
+
             if (item.type == ModContent.ItemType<CeaselessVoidBag>())
-            {
-                var voidInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                voidInfLCR.Add(ModContent.ItemType<DarkPlasma>(), 1, 8, 9);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<DarkPlasma>(), 8);
+
             if (item.type == ModContent.ItemType<SignusBag>())
-            {
-                var signusInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                signusInfLCR.Add(ModContent.ItemType<TwistingNether>(), 1, 8, 9);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<TwistingNether>(), 8);
 
             // Polterghast drops 25 extra ruinous souls.
             if (item.type == ModContent.ItemType<PolterghastBag>())
-            {
-                var polterInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                polterInfLCR.Add(ModContent.ItemType<RuinousSoul>(), 1, 25, 26);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<RuinousSoul>(), 25);
 
-            // The Devourer of Gods drops 400 extra cosmilite bars.
+            // The Devourer of Gods drops 960 extra cosmilite bars.
+            // You will never have to fight DoG more than once for materials.
             if (item.type == ModContent.ItemType<DevourerofGodsBag>())
-            {
-                var dogInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                dogInfLCR.Add(ModContent.ItemType<CosmiliteBar>(), 1, 400, 401);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<CosmiliteBar>(), 960);
 
             // Yharon drops 35 extra yharon soul fragments.
             if (item.type == ModContent.ItemType<YharonBag>())
-            {
-                var yharonInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                yharonInfLCR.Add(ModContent.ItemType<YharonSoulFragment>(), 1, 35, 36);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<YharonSoulFragment>(), 35);
 
             // Draedon drops 40 extra exo prisms.
             if (item.type == ModContent.ItemType<DraedonBag>())
-            {
-                var draedonInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                draedonInfLCR.Add(ModContent.ItemType<ExoPrism>(), 1, 40, 41);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<ExoPrism>(), 40);
 
             // Calamitas drops 40 extra ashes of annihilation.
             if (item.type == ModContent.ItemType<SupremeCalamitasCoffer>())
-            {
-                var calInfLCR = loot.DefineConditionalDropSet(DropHelper.If(() => InfernumMode.CanUseCustomAIs));
-                calInfLCR.Add(ModContent.ItemType<AshesofAnnihilation>(), 1, 40, 41);
-            }
+                addInfernumExclusiveItem(ModContent.ItemType<AshesofAnnihilation>(), 40);
         }
     }
 }

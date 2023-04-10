c0c0e6cb3dfcd79e4fa507e9d0ccedc5cd96bbf0
Dominic <farfetchedking541@gmail.com>
2023-04-10 02:42:13 -0500

    No more cum lava part 2

--- Content/Backgrounds/ProfanedGardenLavaBackground.cs
+++ Content/Backgrounds/ProfanedGardenLavaBackground.cs
@@ -13,7 +13,7 @@ namespace InfernumMode.Content.Backgrounds
             get
             {
                 Color dayColor = Color.Lerp(Color.Orange, Color.Wheat, 0.67f) with { A = 0 } * (CalamityUtils.AnyBossNPCS() ? 0.45f : 1f);
-                Color nightColor = Color.SkyBlue with { A = 0 } * (CalamityUtils.AnyBossNPCS() ? 0.35f : 1f);
+                Color nightColor = Color.Lerp(Color.MediumBlue, Color.Yellow, 0.3f) with { A = 0 } * (CalamityUtils.AnyBossNPCS() ? 0.35f : 1f);
                 float nightInterpolant = 1f;
                 if (Main.dayTime)
                     nightInterpolant = 1f - Utils.GetLerpValue(0f, 1500f, (float)Main.time, true) * Utils.GetLerpValue((float)Main.dayLength, (float)Main.dayLength - 1500f, (float)Main.time, true);

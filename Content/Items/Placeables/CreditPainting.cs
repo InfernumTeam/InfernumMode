83c091037a73008a63b50b4d7581e902ff61e778
Toasty <louscliff2@gmail.com>
2023-04-13 06:49:54 +0100

    added Credit where Credit is Due

---
+++ Content/Items/Placeables/CreditPainting.cs
@@ -0,0 +1,28 @@
+﻿using InfernumMode.Content.Tiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace InfernumMode.Content.Items.Placeables
+{
+    public class CreditPainting : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Credit where Credit is Due");
+            SacrificeTotal = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<CreditPaintingTile>());
+
+            Item.width = 32;
+            Item.height = 32;
+            Item.maxStack = 99;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.buyPrice(0, 1, 0, 0);
+            Item.Infernum_Tooltips().DeveloperItem = true;
+        }
+    }
+}
---
+++ Content/Tiles/CreditPaintingTile.cs
@@ -0,0 +1,41 @@
+﻿using InfernumMode.Content.Items.Placeables;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace InfernumMode.Content.Tiles
+{
+    public class CreditPaintingTile : ModTile
+    {
+        public const int Width = 6;
+        public const int Height = 4;
+
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileWaterDeath[Type] = false;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.Width = 6;
+            TileObjectData.newTile.Height = 4;
+            TileObjectData.newTile.Origin = new Point16(2, 2);
+            TileObjectData.newTile.CoordinateHeights = new int[4] { 16, 16, 16, 16 };
+            TileObjectData.newTile.LavaDeath = true;
+            TileObjectData.addTile(Type);
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+
+            AddMapEntry(new Color(55, 27, 7));
+            DustType = DustID.WoodFurniture;
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 32, ModContent.ItemType<CreditPainting>());
+        }
+    }
+}

0cdab788137bbd11122c6d4e650d79bb2b5d9390
Dominic <farfetchedking541@gmail.com>
2023-04-14 22:01:00 -0500

    Generic death messages

--- Core/GlobalInstances/Systems/TipsManager.cs
+++ Core/GlobalInstances/Systems/TipsManager.cs
@@ -51,10 +51,10 @@ namespace InfernumMode.Core.GlobalInstances.Systems
 
             // This func evaluates the state of the NPC in question, after it died.
             IEnumerable<Func<NPC, string>> potentialTips = bossInfo.GetTips(hatGirl);
-            if (!hatGirl && !potentialTips.Any())
-                potentialTips = potentialTips.Append(n => Main.rand.Next(RisingWarriorsSoulstone.GenericThingsToSayOnDeath));
-
             var possibleThingsToSay = potentialTips.Select(t => t(BossBeingFought)).Where(t => !string.IsNullOrEmpty(t) && !SaidText.Contains(t)).ToList();
+            if (!hatGirl && !possibleThingsToSay.Any())
+                possibleThingsToSay.AddRange(RisingWarriorsSoulstone.GenericThingsToSayOnDeath.Where(s => !SaidText.Contains(s)));
+
             if (potentialTips is null || !potentialTips.Any() || possibleThingsToSay.Count <= 0)
                 return string.Empty;
 

0335fbc48202588d8611ca740003d1eeb843d089
Dominic <farfetchedking541@gmail.com>
2023-08-05 00:46:00 -0500

    Fix Plantera's tentacle attack being unavoidable

---
+++ Content/BehaviorOverrides/BossAIs/Plantera/PlanteraFreeTentacleBehaviorOverride.cs
@@ -0,0 +1,19 @@
+﻿using CalamityMod.NPCs.NormalNPCs;
+using InfernumMode.Core.OverridingSystem;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InfernumMode.Content.BehaviorOverrides.BossAIs.Plantera
+{
+    public class PlanteraFreeTentacleBehaviorOverride : NPCBehaviorOverride
+    {
+        public override int NPCOverrideType => ModContent.NPCType<PlanterasFreeTentacle>();
+
+        public override bool PreAI(NPC npc)
+        {
+            // Nope.
+            npc.active = false;
+            return false;
+        }
+    }
+}

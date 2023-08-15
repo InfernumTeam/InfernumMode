ddd41c290918f5ecd975aedc36c13207c4556492
Toasty <louscliff@gmail.com>
2023-08-15 13:57:14 +0100

    ...

--- Core/GlobalInstances/Players/UIPlayer.cs
+++ Core/GlobalInstances/Players/UIPlayer.cs
@@ -17,7 +17,7 @@ namespace InfernumMode.Core.GlobalInstances.Players
                 mods = $"{InfernumMode.CalamityModMusic.DisplayName}{(InfernumMode.MusicModIsActive ? " & " + InfernumMode.InfernumMusicMod.DisplayName : string.Empty)}";
             else
                 mods = $"{InfernumMode.InfernumMusicMod.DisplayName}";
-            Main.NewText($"[c/b90000:Infernum Mod: You have {mods} mod(s) enabled, these may cause some boss fights to crash.]" +
+            Main.NewText($"[c/b90000:Infernum Mod: You have the {mods} mod(s) enabled, these may cause some boss fights to crash.]" +
                 $"\n[c/b90000:A fix is being worked on, but for the meantime disabling the mod(s) will fix the crashing.]");
         }
     }

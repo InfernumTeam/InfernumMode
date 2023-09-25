e6d17af2ca603d8695506be70c6b72375aa1c689
Blast <oshri040406@gmail.com>
2023-09-26 00:09:19 +0300

    - Added a perf medium worm tip replacement, and put the original in the on death pool.

--- Content/BehaviorOverrides/BossAIs/Perforators/PerforatorHiveBehaviorOverride.cs
+++ Content/BehaviorOverrides/BossAIs/Perforators/PerforatorHiveBehaviorOverride.cs
@@ -1135,7 +1135,7 @@ namespace InfernumMode.Content.BehaviorOverrides.BossAIs.Perforators
             if (attackTimer == wormSummonTime)
             {
                 MakeWormEruptFromHive(npc, -Vector2.UnitY, 1f, ModContent.NPCType<PerforatorHeadMedium>());
-                HatGirl.SayThingWhileOwnerIsAlive(target, "Mods.InfernumMode.PetDialog.PerforatorsProjectilesTip");
+                HatGirl.SayThingWhileOwnerIsAlive(target, "Mods.InfernumMode.PetDialog.PerforatorsWorm2Tip");
             }
 
             // Go to the next attack if the small perforator is dead.
@@ -1600,6 +1600,7 @@ namespace InfernumMode.Content.BehaviorOverrides.BossAIs.Perforators
         public override IEnumerable<Func<NPC, string>> GetTips()
         {
             yield return n => "Mods.InfernumMode.PetDialog.PerforatorsTip1";
+            yield return n => "Mods.InfernumMode.PetDialog.PerforatorsProjectilesTip";
         }
         #endregion Tips
     }

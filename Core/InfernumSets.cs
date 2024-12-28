dad7f201ef0a3e120c1a8eecf509db9645587200
Toasty <louscliff@gmail.com>
2024-12-28 12:59:31 +0000

    - Remove config relating to removed content.

--- Core/InfernumSets.cs
+++ Core/InfernumSets.cs
@@ -9,59 +9,60 @@ using Terraria.ModLoader;
 
 namespace InfernumMode.Core
 {
-    public class InfernumSets : ModSystem
+    public static class InfernumSets
     {
-        public class Items
+        public sealed class Projectiles : ModSystem
         {
+            public static bool[] Tombstone
+            {
+                get;
+                private set;
+            }
 
-        }
-        public class NPCs
-        {
+            public static bool[] RocketIllegalInArena
+            {
+                get;
+                private set;
+            }
 
-        }
-        public class Projectiles
-        {
-            public static bool[] Tombstone;
-            public static bool[] RocketIllegalInArena;
-        }
+            public override void PostSetupContent()
+            {
+                Tombstone = ProjectileID.Sets.Factory.CreateBoolSet(false,
+                    ProjectileID.Tombstone,
+                    ProjectileID.Gravestone,
+                    ProjectileID.RichGravestone1,
+                    ProjectileID.RichGravestone2,
+                    ProjectileID.RichGravestone3,
+                    ProjectileID.RichGravestone4,
+                    ProjectileID.RichGravestone4,
+                    ProjectileID.Headstone,
+                    ProjectileID.Obelisk
+                    );
 
-        public override void PostSetupContent()
-        {
-            SetFactory projFactory = ProjectileID.Sets.Factory;
-            Projectiles.Tombstone = projFactory.CreateBoolSet(false,
-                ProjectileID.Tombstone, 
-                ProjectileID.Gravestone, 
-                ProjectileID.RichGravestone1, 
-                ProjectileID.RichGravestone2,
-                ProjectileID.RichGravestone3, 
-                ProjectileID.RichGravestone4, 
-                ProjectileID.RichGravestone4, 
-                ProjectileID.Headstone, 
-                ProjectileID.Obelisk
-                );
-            Projectiles.RocketIllegalInArena = projFactory.CreateBoolSet(false,
-                ProjectileID.DryRocket,
-                ProjectileID.DryGrenade,
-                ProjectileID.DryMine,
-                ProjectileID.DrySnowmanRocket,
-                ProjectileID.WetRocket,
-                ProjectileID.WetGrenade,
-                ProjectileID.WetMine,
-                ProjectileID.WetBomb,
-                ProjectileID.WetSnowmanRocket,
-                ProjectileID.HoneyRocket,
-                ProjectileID.HoneyGrenade,
-                ProjectileID.HoneyMine,
-                ProjectileID.HoneyBomb,
-                ProjectileID.HoneySnowmanRocket,
-                ProjectileID.LavaRocket,
-                ProjectileID.LavaGrenade,
-                ProjectileID.LavaMine,
-                ProjectileID.LavaBomb,
-                ProjectileID.LavaSnowmanRocket,
-                ProjectileID.DirtBomb,
-                ProjectileID.DirtStickyBomb
-                );
+                RocketIllegalInArena = ProjectileID.Sets.Factory.CreateBoolSet(false,
+                    ProjectileID.DryRocket,
+                    ProjectileID.DryGrenade,
+                    ProjectileID.DryMine,
+                    ProjectileID.DrySnowmanRocket,
+                    ProjectileID.WetRocket,
+                    ProjectileID.WetGrenade,
+                    ProjectileID.WetMine,
+                    ProjectileID.WetBomb,
+                    ProjectileID.WetSnowmanRocket,
+                    ProjectileID.HoneyRocket,
+                    ProjectileID.HoneyGrenade,
+                    ProjectileID.HoneyMine,
+                    ProjectileID.HoneyBomb,
+                    ProjectileID.HoneySnowmanRocket,
+                    ProjectileID.LavaRocket,
+                    ProjectileID.LavaGrenade,
+                    ProjectileID.LavaMine,
+                    ProjectileID.LavaBomb,
+                    ProjectileID.LavaSnowmanRocket,
+                    ProjectileID.DirtBomb,
+                    ProjectileID.DirtStickyBomb
+                    );
+            }
         }
     }
 }

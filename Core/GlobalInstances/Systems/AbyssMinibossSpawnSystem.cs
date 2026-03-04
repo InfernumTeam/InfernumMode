2e5356d47425a5f569c302a828ff639698e39904
Habble53 <87115317+Habble53@users.noreply.github.com>
2026-03-04 14:54:34 +0300

    Infernum abyss Reaper Shark and Eidolon Wyrm miniboss spawns now cycle through each other rather than choose either at random

--- Core/GlobalInstances/Systems/AbyssMinibossSpawnSystem.cs
+++ Core/GlobalInstances/Systems/AbyssMinibossSpawnSystem.cs
@@ -28,6 +28,7 @@ namespace InfernumMode.Core.GlobalInstances.Systems
         }
 
         public const int MinibossSpawnRate = 2400;
+        private static bool CycleThroughMiniboss;
 
         public override void PreUpdateWorld()
         {
@@ -35,16 +36,15 @@ namespace InfernumMode.Core.GlobalInstances.Systems
             if (!WorldSaveSystem.InPostAEWUpdateWorld || MajorAbyssEnemyExists || !InfernumMode.CanUseCustomAIs)
                 return;
 
-            for (int i = 0; i < Main.maxPlayers; i++)
+            foreach (Player p in Main.ActivePlayers)
             {
-                Player p = Main.player[i];
-                if (p.dead || !p.active)
+                if (p == null || p.dead || p.ghost)
                     continue;
 
                 if (!p.Calamity().ZoneAbyssLayer4 || p.Center.Y < CustomAbyss.Layer4Top * 16f + 1000f)
                     continue;
 
-                if (!Main.rand.NextBool(MinibossSpawnRate))
+                if (!Main.rand.NextBool(MinibossSpawnRate) || typeof(NPC).GetField("maxSpawns", LumUtils.UniversalBindingFlags)?.GetValue(null) as int? == 0)
                     continue;
 
                 AttemptToSpawnMiniboss(p);
@@ -55,7 +55,8 @@ namespace InfernumMode.Core.GlobalInstances.Systems
         public static void AttemptToSpawnMiniboss(Player player)
         {
             // Spawn a miniboss a preset distance away from the target.
-            int minibossID = Main.rand.NextFromList(ModContent.NPCType<ReaperShark>(), ModContent.NPCType<EidolonWyrmHead>());
+            int minibossID = CycleThroughMiniboss ? ModContent.NPCType<ReaperShark>() : ModContent.NPCType<EidolonWyrmHead>();
+            CycleThroughMiniboss = !CycleThroughMiniboss;
             Vector2 minibossSpawnPosition = player.Center + Main.rand.NextVector2CircularEdge(1080f, 1080f);
 
             int miniboss = NPC.NewNPC(new EntitySource_WorldEvent(), (int)minibossSpawnPosition.X, (int)minibossSpawnPosition.Y, minibossID, 1);
